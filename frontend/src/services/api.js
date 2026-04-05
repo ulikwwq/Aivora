a0d25227256bd8ba7e263c5cbdd1ac2c6d844731
ulikwwq <ulikwwq@gmail.com>
2026-04-05 22:29:15 +0600

    Update API URL to production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080';
+const API_URL = 'https://aivora-backend-l8mv.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
